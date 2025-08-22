7faa9821ca5330a836a4d9eb31b117796136ef9d
nelimalu <lukaj2501@gmail.com>
2025-08-22 01:09:01 -0400

    created panel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import Navbar from '../components/Navbar';
 import Search from '../components/Search';
+import Panel from '../components/Panel';
+import './Home.css';
 
 function Home() {
   return (
     <div>
       <Navbar />
       <Search />
+      <div className="panels-container">
+        <Panel />
+        <Panel />
+        <Panel />
+      </div>
     </div>
   );
 }
