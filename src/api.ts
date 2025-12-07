fabbb58459e38301cf59cb10afd8a397ba5d64c5
nelimalu <lukaj2501@gmail.com>
2025-12-07 00:24:57 -0500

    updated to ngrok link

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://unmutative-countercurrently-leopoldo.ngrok-free.dev";
 // console.log("API Base URL:", API_BASE_URL);
 
 interface LoginPayload {
